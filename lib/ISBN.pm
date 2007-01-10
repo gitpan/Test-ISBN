145649a3bc1ddf2e60e858602d1e746c4a1c1bec
brian d foy <bdfoy@cpan.org>
2007-01-10 04:52:34 +0000

    Import of BDFOY/Test-ISBN-1.09 from CPAN.
    gitpan-cpan-distribution: Test-ISBN
    gitpan-cpan-version:      1.09
    gitpan-cpan-path:         BDFOY/Test-ISBN-1.09.tar.gz
    gitpan-cpan-author:       BDFOY
    gitpan-cpan-maturity:     released

--- lib/ISBN.pm
+++ lib/ISBN.pm
@@ -1,4 +1,4 @@
-# $Id: ISBN.pm,v 1.8 2006/11/09 12:52:46 comdog Exp $
+# $Id: ISBN.pm,v 1.9 2007/01/10 04:50:39 comdog Exp $
 package Test::ISBN;
 use strict;
 
@@ -11,7 +11,7 @@ use Test::Builder;
 
 my $Test = Test::Builder->new();
 
-$VERSION = sprintf "%d.%02d", q$Revision: 1.8 $ =~ m/ (\d+) \. (\d+) /xg;
+$VERSION = sprintf "%d.%02d", q$Revision: 1.9 $ =~ m/ (\d+) \. (\d+) /xg;
 @EXPORT  = qw(isbn_ok isbn_country_ok isbn_publisher_ok);
 
 =head1 NAME
@@ -129,13 +129,14 @@ members of the project can shepherd this module appropriately.
 
 brian d foy, C<< <bdfoy@cpan.org> >>
 
-=head1 COPYRIGHT
+=head1 COPYRIGHT AND LICENSE
 
-Copyright 2002-2006 brian d foy, All rights reserved.
+Copyright (c) 2002-2007 brian d foy.  All rights reserved.
 
-You can use this module under the same terms as
-Perl itself.
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
 
 =cut
+
 
 1;
